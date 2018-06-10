d29395bbeab33f94a40ebef3c8111a11ada33025
BriceWu <1003800829@qq.com>
2018-06-10 11:39:57 +0800

    网格显示

--- dPic.py
+++ dPic.py
@@ -1,20 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from matplotlib.ticker import MultipleLocator
 
 # 将x主刻度标签设置为0.01的倍数
-xmajorLocator = MultipleLocator(0.05)
+x_major_Locator = MultipleLocator(0.05)
 # 将y轴主刻度标签设置为0.01的倍数
-ymajorLocator = MultipleLocator(0.05)
+y_major_Locator = MultipleLocator(0.05)
 
 plt.figure(1)
+plt.grid(True, linestyle="-", color="b", linewidth="0.5")
 ax = plt.subplot(111)
 # 在0到1之间，均匀产生100点的数组
 x = np.linspace(0, 1, 100)
+ax.plot()
 ax.plot(x, -x * np.log2(x))
 
 # 设置主刻度标签的位置,标签文本的格式
-ax.xaxis.set_major_locator(xmajorLocator)
-ax.yaxis.set_major_locator(ymajorLocator)
+ax.xaxis.set_major_locator(x_major_Locator)
+ax.yaxis.set_major_locator(y_major_Locator)
 
 plt.show()
