ed1bf61657bf70bc241cdb276219d49a518ee76b
BriceWu <1003800829@qq.com>
2018-06-10 11:30:14 +0800

    自定义刻度

--- dPic.py
+++ dPic.py
@@ -1,17 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+# 将x主刻度标签设置为0.01的倍数
+xmajorLocator = MultipleLocator(0.05)
+# 将y轴主刻度标签设置为0.01的倍数
+ymajorLocator = MultipleLocator(0.05)
 
 plt.figure(1)
 ax = plt.subplot(111)
-x = np.linspace(0, np.pi * 2, 200)  # 在0到2pi之间，均匀产生200点的数组
+# 在0到1之间，均匀产生100点的数组
+x = np.linspace(0, 1, 100)
+ax.plot(x, -x * np.log2(x))
 
-# r = 2cosθ
-r = 2 * np.cos(x)  # 半径
-ax.plot(r * np.cos(x), r * np.sin(x))
-
-# r = 1
-r = 1
-ax.plot(r * np.cos(x), r * np.sin(x))
-
+# 设置主刻度标签的位置,标签文本的格式
+ax.xaxis.set_major_locator(xmajorLocator)
+ax.yaxis.set_major_locator(ymajorLocator)
 
 plt.show()
